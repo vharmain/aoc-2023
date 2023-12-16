e7cd1f0f135beff21e30ebc2be90010f9327aa42
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2023-12-16 21:56:22 +0200

    Day 05 part 2

--- src/day_05.clj
+++ src/day_05.clj
@@ -11,7 +11,7 @@
      {:seeds (-> line1
                  (str/split #":")
                  second
-                 (->> (format "#{%s}")
+                 (->> (format "[%s]")
                       (read-string)))}
      (-> lines
          (->> (remove empty?)
@@ -49,6 +49,8 @@
            :temperature-to-humidity
            :humidity-to-location])
 
+;;; Part 1 ;;;
+
 (-> data
     :seeds
     (->> (map #(traverse data {} path %))
@@ -56,3 +58,45 @@
          (remove nil?)
          (apply min)))
 ;; => 88151870
+
+;;; Part 2 ;;;
+
+(def locations-sorted
+  (->> data
+       :humidity-to-location
+       (sort-by :destination-range-start)))
+
+(def seed-ranges
+  (->> data
+       :seeds
+       (partition 2)
+       (map (fn [[range-start range-length]]
+              {:range-start  range-start
+               :range-length range-length}))))
+
+(defn traverse-backward
+  [m res path n]
+  (let [k  (first path)
+        m1 (some (fn [{:keys [source-range-start range-length destination-range-start] :as m1}]
+                   (when (>= (dec (+ destination-range-start range-length)) n destination-range-start)
+                     (let [output (+ source-range-start (- n destination-range-start))]
+                       (assoc m1 :input n :output output))))
+                 (get m k))]
+    (if (and m1 (seq path))
+      (recur m (assoc res k m1) (rest path) (:output m1))
+      res)))
+
+(-> locations-sorted
+    (->> (mapcat (fn [{:keys [destination-range-start range-length]}]
+                   (range destination-range-start (+ destination-range-start range-length))))
+         (map #(traverse-backward data {} (reverse path) %))
+         (filter (fn [{:keys [seed-to-soil] :as res}]
+                   (let [n (:output seed-to-soil)]
+                     (some (fn [{:keys [range-start range-length]}]
+                             (when (>= (dec (+ range-start range-length)) n range-start)
+                               (assoc res :seed (+ range-start (- n range-start)))))
+                           seed-ranges))))
+         first)
+    :humidity-to-location
+    :input)
+;; => 2008785
